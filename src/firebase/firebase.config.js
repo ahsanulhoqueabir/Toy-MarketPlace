587d8e3592e868a82a4bec26487423a64bdfb38f
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-01-31 00:52:06 +0600

    environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAMhR__lQuXwsQD-IrVY3Bl0Dh_NiJn93c",
-  authDomain: "toy-market-84c26.firebaseapp.com",
-  projectId: "toy-market-84c26",
-  storageBucket: "toy-market-84c26.appspot.com",
-  messagingSenderId: "868748127609",
-  appId: "1:868748127609:web:c40ed81fe68b0648f8d3a8",
+  apiKey: import.meta.env.VITE_APIKEY_APIKEY,
+  authDomain: import.meta.env.VITE_APIKEY_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APIKEY_PROJECTID,
+  storageBucket: import.meta.env.VITE_APIKEY_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APIKEY_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APIKEY_APPID,
 };
 
 // Initialize Firebase
