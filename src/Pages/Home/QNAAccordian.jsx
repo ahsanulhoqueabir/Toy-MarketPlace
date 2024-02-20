133624fe83e675d548a64123b98999284b663880
MD AHSANUL HOQUE ABIR <113261318+ahsanulhoqueabir@users.noreply.github.com>
2024-02-20 08:51:05 +0600

    QNA section added

--- src/Pages/Home/QNAAccordian.jsx
+++ src/Pages/Home/QNAAccordian.jsx
@@ -8,6 +8,28 @@ const accordionData = [
     subtitle:
       " Now you can find here only scientific category includes subcategory of Astronomy,chemsitry and Magnetics",
   },
+  {
+    title: "What all products I can purchase in the Toys & Games category?",
+    subtitle: "You can purchase only scientific toys and games",
+  },
+  {
+    title: "Why choose AtomAlly?",
+    subtitle:
+      "AtomAlly is a platform that provides a wide range of scientific toys and games. It is a one-stop destination for all your scientific toys and games needs. You can find a variety of scientific toys and games from different brands and sellers. You can choose from a wide range of products and get the best deals. AtomAlly offers a convenient and hassle-free shopping experience. You can shop for scientific toys and games from the comfort of your home. You can also avail of fast delivery and easy returns. AtomAlly offers secure payment options and easy order tracking. You can also enjoy exciting discounts and offers on your purchase.",
+  },
+  {
+    title: "Do you Offer Free Shipping?",
+    subtitle: "Yes, we offer free shipping on all orders above $100",
+  },
+  {
+    title: " How long would it take for me to received my Order?",
+    subtitle:
+      "We offer express shipment on all orders. Once you place an order with us, we ship it within 3-10 working days and receipt of payment is also provided to you.",
+  },
+  {
+    title: "What is the return policy?",
+    subtitle: "You can return the product within 30 days of purchase",
+  },
 ];
 const QNAAccordian = () => {
   const [isActive, setIsActive] = useState(0);
@@ -15,11 +37,17 @@ const QNAAccordian = () => {
     setIsActive((prevIdx) => (prevIdx === idx ? null : idx));
   };
   return (
-    <div className="pt-10 mt-10 bg-teal-200">
+    <div className="pt-10 mt-10 bg-teal-100">
       <h2 className="text-3xl text-center font-bold py-10">QnA Section</h2>
       <div className="space-y-4 p-2 md:p-6">
         {accordionData?.map((data, idx) => (
-          <div key={idx}>
+          <div
+            data-aos="zoom-in"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            key={idx}
+          >
             {/* header / Title */}
             <div
               onClick={() => handleToggle(idx)}
